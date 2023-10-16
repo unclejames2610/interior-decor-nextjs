a2bc4eb87ca10c074e29918905f8b70a6148e735
unclejames2610 <udealaebuka@gmail.com>
2023-10-16 15:53:44 +0100

    feat:done with reference page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import HeroPage from "./hero/page";
 import ProductsPage from "./products/page";
+import ReferencePage from "./reference/page";
 import ServicesPage from "./services/page";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <HeroPage />
       <ServicesPage />
       <ProductsPage />
+      <ReferencePage />
     </div>
   );
 }
--- app/reference/page.tsx
+++ app/reference/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const ReferencePage = () => {
   return (
-    <div>
+    <div id="reference">
       <Reference />
     </div>
   );
